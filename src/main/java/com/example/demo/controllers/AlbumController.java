b524f2b38c08dc209365725f8093b07ba540d6cc
Mateus Selani <selaneitor@gmail.com>
2022-06-16 18:46:48 -0300

    album - repository, service e controller

--- src/main/java/com/example/demo/controllers/AlbumController.java
+++ src/main/java/com/example/demo/controllers/AlbumController.java
@@ -1,13 +1,57 @@
 package com.example.demo.controllers;
 
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class AlbumController {
+import com.example.demo.models.Album;
+import com.example.demo.services.AlbumService;
 
+@RestController
+@RequestMapping("/album")
+public class AlbumController {
+	
+	@Autowired
+	AlbumService as;
+	
 	@GetMapping
-	public String teste() {
-		return "hello";
+	@ResponseBody
+	public List<Album> listarAlbuns() {
+		return as.listarTodos();
 	}
+	
+	@GetMapping
+	@ResponseBody
+	public Album listarAlbumId(@PathVariable UUID idAlbum) {
+		return as.listarUnico(idAlbum);
+	}
+	
+	@PostMapping
+	@ResponseBody
+	public Album salvarAlbum(@PathVariable Album novoAlbum) {
+		return as.salvar(novoAlbum);
+	}
+	
+	@PutMapping
+	@ResponseBody
+	public Album alterarAlbum(@PathVariable UUID idAlbum, Album novoAlbum) {
+		return as.alterar(idAlbum, novoAlbum);
+	}
+	
+	@DeleteMapping
+	@ResponseBody
+	public String deletarAlbum(@PathVariable UUID idAlbum) {
+		as.deletar(idAlbum);
+		return "Album: " + idAlbum + " Deletado!";
+	}
+	
 }
---
+++ src/main/java/com/example/demo/services/AlbumService.java
@@ -0,0 +1,39 @@
+package com.example.demo.services;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.demo.models.Album;
+import com.example.demo.repositories.IAlbumRepository;
+
+public class AlbumService {
+	
+	@Autowired
+	IAlbumRepository ar;
+	
+	public List<Album> listarTodos() {
+		return ar.findAll();
+	}
+	
+	public Album listarUnico(UUID id) {
+		return ar.findById(id).get();
+	}
+	
+	public Album salvar(Album novo) {
+		return ar.save(novo);
+	}
+	
+	public Album alterar(UUID id, Album alterado) {
+		Album albumAntigo = listarUnico(id);
+		alterado.setIdAlbum(albumAntigo.getIdAlbum());
+		albumAntigo = alterado;
+		return alterado;
+	}
+	
+	public void deletar(UUID id) {
+		ar.deleteById(id);
+	}
+
+}
